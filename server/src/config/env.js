da2f955a69c537495d09095a9bfa7ac0f49b244b
abhinav <abhinavisgreat18@gmail.com>
2026-06-11 01:45:30 +0530

    feat: added env.js

---
+++ server/src/config/env.js
@@ -0,0 +1,27 @@
+const dotenv = require("dotenv");
+const z = require("zod");
+
+dotenv.config({ quite: true });
+
+const envSchema = z.object({
+	PORT: z.coerce.number().default(),
+	MONGODB_URI: z.string().default(),
+	JWT_SECRET_ACCESS: z.string(),
+	JWT_SECRET_REFRESH: z.string(),
+	NODE_ENV: z.string(),
+	LOGGER_LEVEL: z.string().default(constants.LOGGER_LEVEL),
+	CORS_ORIGIN: z.string(),
+	RATELIMIT_WINDOWMS: z.coerce.number().default(constants.RATELIMIT_WINDOWMS),
+	RATELIMIT: z.coerce.number().default(constants.RATELIMIT),
+	GOOGLE_CLIENT_ID: z.string(),
+	GOOGLE_CLIENT_SECRET: z.string(),
+	GOOGLE_CALLBACK_URL: z.string(),
+});
+
+const parsed = z.safeParse(process.env);
+
+if (!parsed.success) {
+	console.log("Check your Env");
+}
+
+module.exports = parsed.data;
