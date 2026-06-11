43c7272e07dbad5e49923b7a19af8c0fdc610885
abhinav <abhinavisgreat18@gmail.com>
2026-06-11 23:10:36 +0530

    update: added app_config in app.constant.js and updated env.js

--- server/src/constants/app.constant.js
+++ server/src/constants/app.constant.js
@@ -1,8 +1,38 @@
-module.exports = {
+const constants = {
 	PORT: 3000,
 	MONGO_URI: "mongodb://localhost:27017/crickbuzz",
 	NODE_ENV: "development",
 	LOGGER_LEVEL: "info",
 	RATELIMIT_WINDOWMS: 15 * 60 * 1000,
 	RATELIMIT: 1000,
+};
+
+const app_config = {
+	jwt: {
+		refreshToken: {
+			expiresIn: "30D",
+		},
+		accessToken: {
+			expiresIn: "1H",
+		},
+	},
+	cookie: {
+		accessToken: {
+			httpOnly: true,
+			secure: false,
+			sameSite: "lax",
+			maxAge: 60 * 60 * 1000,
+		},
+		refreshToken: {
+			httpOnly: true,
+			secure: false,
+			sameSite: "lax",
+			maxAge: 30 * 24 * 60 * 60 * 1000,
+		},
+	},
+};
+
+module.exports = {
+	constants,
+	app_config,
 };
