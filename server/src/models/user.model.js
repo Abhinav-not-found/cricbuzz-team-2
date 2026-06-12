cdf746d10c6d0b3edf995eda8860a26d2a5fcfd2
abhinav <abhinavisgreat18@gmail.com>
2026-06-12 16:30:40 +0530

    fix: user.model,  updated appConstants

--- server/src/models/user.model.js
+++ server/src/models/user.model.js
@@ -1,38 +1,38 @@
-const mognoose = require('mongoose')
-const ROLES = require('../constants/model.constant')
-const { boolean } = require('zod')
-const { default: mongoose } = require('mongoose')
+const mongoose = require("mongoose");
+const ROLES = require("../constants/model.constant");
 
-const userSchema = new mognoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-        lowercase: true,
-        trim: true
-    },
-    password: {
-        type: String,
-    },
-    role: {
-        type: String,
-        enum: Object.values(ROLES),
-        default: ROLES
-    },
-    isDeleted: {
-        type: Boolean,
-        default: false
-    }
-},{
-    timestamps: true
-}
-)
+const userSchema = new mongoose.Schema(
+	{
+		name: {
+			type: String,
+			required: true,
+			trim: true,
+		},
+		email: {
+			type: String,
+			required: true,
+			unique: true,
+			lowercase: true,
+			trim: true,
+		},
+		password: {
+			type: String,
+		},
+		role: {
+			type: String,
+			enum: Object.values(ROLES),
+			default: ROLES,
+		},
+		isDeleted: {
+			type: Boolean,
+			default: false,
+		},
+	},
+	{
+		timestamps: true,
+	},
+);
 
-const UserModel = mongoose.model('User', userSchema)
+const UserModel = mongoose.model("User", userSchema);
 
-module.exports = UserModel
+module.exports = UserModel;
