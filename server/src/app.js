53a3fac42a0845a07a0b536e2b59cb8512f0922b
Aman Sharma <sharma9917375201@gmail.com>
2026-06-12 12:21:04 +0530

    feat: add 404 not found middleware

--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const errorHandlerMiddleware = require("./middleware/errorHandler.middleware");
+const handleNotFound = require("./shared/error/notFound.error");
 
 function createServer() {
 	const app = express();
 
+	app.use(handleNotFound);
 	app.use(errorHandlerMiddleware);
 	return app;
 }
