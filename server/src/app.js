64ed84b91b864390502c519ad5559a66c6a74686
vatsarun-dev <vatsarun58@gmail.com>
2026-06-13 11:51:10 +0530

    adding the user registeration, login and get fucntionality with JWT token

--- server/src/app.js
+++ server/src/app.js
@@ -1,18 +1,19 @@
 const express = require("express");
-const env = require('./config/env')
-const morgan = require('morgan');
+const env = require("./config/env");
+const morgan = require("morgan");
 const securityMiddleware = require("./middlewares/security.middleware");
+const router = require("./routes/user.routes.js");
 
 function createServer() {
-	const app = express();
+  const app = express();
 
-	securityMiddleware(app)
-	
-	if(env.NODE_ENV === 'development') {
-		app.use(morgan('dev'));
-	}
+  securityMiddleware(app);
 
-	return app;
+  if (env.NODE_ENV === "development") {
+    app.use(morgan("dev"));
+  }
+  app.use("/api/user", router);
+  return app;
 }
 
 module.exports = createServer;
