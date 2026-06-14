ada4f92ed75df97df926562dda4fb00263e97377
vatsarun-dev <vatsarun58@gmail.com>
2026-06-14 14:35:24 +0530

    adding some functionality

--- server/src/app.js
+++ server/src/app.js
@@ -7,6 +7,7 @@ const env = require("./config/env");
 const securityMiddleware = require("./middlewares/security.middleware");
 const PassportConfig = require("./config/passport");
 const authRoutes = require("./modules/auth/auth.route");
+const userRoutes = require("./modules/user/user.route");
 
 function createServer() {
 	const app = express();
@@ -25,6 +26,7 @@ function createServer() {
 	});
 	app.use("/api/auth", authRoutes);
 	app.use("/api/player", playerRoutes);
+	app.use("/api/users", userRoutes);
 	app.use("/api/users", userRoutes);
 
 	app.use(NotFoundHandler.handle);
