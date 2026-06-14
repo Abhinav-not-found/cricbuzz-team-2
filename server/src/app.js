c035dfa390211b06b1d0ecad40dc8281d1fa252b
vatsarun-dev <vatsarun58@gmail.com>
2026-06-14 14:35:24 +0530

    adding some functionality

--- server/src/app.js
+++ server/src/app.js
@@ -7,6 +7,7 @@ const env = require("./config/env");
 const securityMiddleware = require("./middlewares/security.middleware");
 const PassportConfig = require("./config/passport");
 const authRoutes = require("./modules/auth/auth.route");
+const userRoutes = require("./modules/user/user.route");
 
 function createServer() {
 	const app = express();
@@ -24,6 +25,7 @@ function createServer() {
 		res.send("CricBuzz backend is running");
 	});
 	app.use("/api/auth", authRoutes);
+	app.use("/api/users", userRoutes);
 
 	app.use(NotFoundHandler.handle);
 	app.use(ErrorHandlerMiddleware.handle);
