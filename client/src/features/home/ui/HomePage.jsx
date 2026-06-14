ff00752c297582dd214742645d1d8a5212a516db
abhinav <abhinavisgreat18@gmail.com>
2026-06-14 22:52:40 +0530

    updated

--- client/src/features/home/ui/HomePage.jsx
+++ client/src/features/home/ui/HomePage.jsx
@@ -4,6 +4,13 @@ const HomePage = () => {
   return (
     <div>
       <Navbar />
+      <h1>Listing of series</h1>
+      <div>
+        list of series
+        <p>series 1</p>
+        <p>series 2</p>
+        <p>series 3</p>
+      </div>
     </div>
   )
 }
