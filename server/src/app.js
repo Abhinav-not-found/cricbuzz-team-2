610d710d706110a687809f8d6a6323ff032c73bd
abhinav <abhinavisgreat18@gmail.com>
2026-06-14 15:41:24 +0530

    feat: added team apis

--- server/src/app.js
+++ server/src/app.js
@@ -8,6 +8,7 @@ const securityMiddleware = require("./middlewares/security.middleware");
 const PassportConfig = require("./config/passport");
 const authRoutes = require("./modules/auth/auth.route");
 const userRoutes = require("./modules/user/user.route");
+const playerRoutes = require("./modules/player/player.route");
 
 function createServer() {
 	const app = express();
@@ -26,15 +27,7 @@ function createServer() {
 	});
 	app.use("/api/auth", authRoutes);
 	app.use("/api/users", userRoutes);
-<<<<<<< HEAD
 	app.use("/api/player", playerRoutes);
-	app.use("/api/users", userRoutes);
-=======
->>>>>>> c035dfa (adding some functionality)
-	app.use("/api/users", userRoutes);
-	app.use("/api/player", playerRoutes);
-	app.use("/api/users", userRoutes);
-	app.use("/api/users", userRoutes);
 
 	app.use(NotFoundHandler.handle);
 	app.use(ErrorHandlerMiddleware.handle);
---
+++ server/src/modules/team/team.service.js
@@ -0,0 +1,98 @@
+const ApiError = require("../../shared/utils/ApiError");
+const TeamRepository = require("../../repository/team.repository");
+
+class TeamService {
+	constructor() {
+		this.teamRepository = new TeamRepository();
+	}
+
+	async createTeam(data, userId) {
+		const existingTeam = await this.teamRepository.findByName(data.name);
+
+		if (existingTeam) {
+			throw new ApiError(409, "Team already exists");
+		}
+
+		return this.teamRepository.create({
+			...data,
+			createdBy: userId,
+			updatedBy: userId,
+		});
+	}
+
+	async getTeams() {
+		return this.teamRepository.findAll();
+	}
+
+	async getTeam(id) {
+		const team = await this.teamRepository.findById(id);
+
+		if (!team) {
+			throw new ApiError(404, "Team not found");
+		}
+
+		return team;
+	}
+
+	async updateTeam(id, data, userId) {
+		const team = await this.teamRepository.findById(id);
+
+		if (!team) {
+			throw new ApiError(404, "Team not found");
+		}
+
+		return this.teamRepository.update(id, {
+			...data,
+			updatedBy: userId,
+		});
+	}
+
+	async deleteTeam(id, userId) {
+		const team = await this.teamRepository.findById(id);
+
+		if (!team) {
+			throw new ApiError(404, "Team not found");
+		}
+
+		return this.teamRepository.update(id, {
+			isDeleted: true,
+			updatedBy: userId,
+		});
+	}
+
+	async addPlayers(teamId, playerIds, userId) {
+		const team = await this.teamRepository.findById(teamId);
+
+		if (!team) {
+			throw new ApiError(404, "Team not found");
+		}
+
+		const updatedPlayers = [
+			...new Set([...team.squadPlayers.map(String), ...playerIds]),
+		];
+
+		return this.teamRepository.update(teamId, {
+			squadPlayers: updatedPlayers,
+			updatedBy: userId,
+		});
+	}
+
+	async removePlayers(teamId, playerIds, userId) {
+		const team = await this.teamRepository.findById(teamId);
+
+		if (!team) {
+			throw new ApiError(404, "Team not found");
+		}
+
+		const updatedPlayers = team.squadPlayers.filter(
+			(player) => !playerIds.includes(player.toString()),
+		);
+
+		return this.teamRepository.update(teamId, {
+			squadPlayers: updatedPlayers,
+			updatedBy: userId,
+		});
+	}
+}
+
+module.exports = TeamService;
