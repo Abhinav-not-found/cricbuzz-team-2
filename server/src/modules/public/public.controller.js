839d0620465a138e60132ae5d17087acbc779c63
abhinav <abhinavisgreat18@gmail.com>
2026-06-15 17:15:38 +0530

    feat: add public routes

---
+++ server/src/modules/public/public.controller.js
@@ -0,0 +1,142 @@
+const httpStatus = require("http-status-codes");
+const PublicService = require("./public.service");
+
+class PublicController {
+	constructor() {
+		this.publicService = new PublicService();
+	}
+
+	async getSeriesList(req, res) {
+		const data = await this.publicService.getSeriesList();
+
+		return res.status(httpStatus.StatusCodes.OK).json({
+			success: true,
+			data,
+		});
+	}
+
+	async getSeriesById(req, res) {
+		const data = await this.publicService.getSeriesById(req.params.seriesId);
+
+		return res.status(httpStatus.StatusCodes.OK).json({
+			success: true,
+			data,
+		});
+	}
+
+	async getMatches(req, res) {
+		const data = await this.publicService.getMatches();
+
+		return res.status(httpStatus.StatusCodes.OK).json({
+			success: true,
+			data,
+		});
+	}
+
+	async getLiveMatches(req, res) {
+		const data = await this.publicService.getLiveMatches();
+
+		return res.status(httpStatus.StatusCodes.OK).json({
+			success: true,
+			data,
+		});
+	}
+
+	async getUpcomingMatches(req, res) {
+		const data = await this.publicService.getUpcomingMatches();
+
+		return res.status(httpStatus.StatusCodes.OK).json({
+			success: true,
+			data,
+		});
+	}
+
+	async getRecentMatches(req, res) {
+		const data = await this.publicService.getRecentMatches();
+
+		return res.status(httpStatus.StatusCodes.OK).json({
+			success: true,
+			data,
+		});
+	}
+
+	async getMatchById(req, res) {
+		const data = await this.publicService.getMatchById(req.params.matchId);
+
+		return res.status(httpStatus.StatusCodes.OK).json({
+			success: true,
+			data,
+		});
+	}
+
+	async getTeams(req, res) {
+		const data = await this.publicService.getTeams();
+
+		return res.status(httpStatus.StatusCodes.OK).json({
+			success: true,
+			data,
+		});
+	}
+
+	async getTeamById(req, res) {
+		const data = await this.publicService.getTeamById(req.params.teamId);
+
+		return res.status(httpStatus.StatusCodes.OK).json({
+			success: true,
+			data,
+		});
+	}
+
+	async getPlayers(req, res) {
+		const data = await this.publicService.getPlayers();
+
+		return res.status(httpStatus.StatusCodes.OK).json({
+			success: true,
+			data,
+		});
+	}
+
+	async getPlayerById(req, res) {
+		const data = await this.publicService.getPlayerById(req.params.playerId);
+
+		return res.status(httpStatus.StatusCodes.OK).json({
+			success: true,
+			data,
+		});
+	}
+
+	async getMatchScore(req, res) {
+		const { matchId } = req.params;
+
+		const data = await this.publicService.getMatchScore(matchId);
+
+		return res.status(200).json({
+			success: true,
+			data,
+		});
+	}
+
+	async getMatchScorecard(req, res) {
+		const { matchId } = req.params;
+
+		const data = await this.publicService.getMatchScorecard(matchId);
+
+		return res.status(200).json({
+			success: true,
+			data,
+		});
+	}
+
+	async getMatchCommentary(req, res) {
+		const { matchId } = req.params;
+
+		const data = await this.publicService.getMatchCommentary(matchId);
+
+		return res.status(200).json({
+			success: true,
+			data,
+		});
+	}
+}
+
+module.exports = PublicController;
---
+++ server/src/repository/public.repository.js
@@ -0,0 +1,93 @@
+const Match = require("../models/match.model");
+const Series = require("../models/series.model");
+const Team = require("../models/team.model");
+const Player = require("../models/player.model");
+const { MATCH_STATUS } = require("../constants/model.constant");
+
+class PublicRepository {
+	async getSeriesList() {
+		return Series.find().sort({ createdAt: -1 });
+	}
+
+	async getSeriesById(seriesId) {
+		return Series.findById(seriesId).populate("matches");
+	}
+
+	async getMatches() {
+		return Match.find()
+			.populate("teamA")
+			.populate("teamB")
+			.sort({ matchDate: -1 });
+	}
+
+	async getLiveMatches() {
+		return Match.find({
+			status: MATCH_STATUS.LIVE,
+		})
+			.populate("teamA")
+			.populate("teamB");
+	}
+
+	async getUpcomingMatches() {
+		return Match.find({
+			status: MATCH_STATUS.UPCOMING,
+		})
+			.populate("teamA")
+			.populate("teamB");
+	}
+
+	async getRecentMatches() {
+		return Match.find({
+			status: MATCH_STATUS.COMPLETED,
+		})
+			.populate("teamA")
+			.populate("teamB")
+			.sort({ updatedAt: -1 });
+	}
+
+	async getMatchById(matchId) {
+		return Match.findById(matchId)
+			.populate("teamA")
+			.populate("teamB")
+			.populate("playing11.player");
+	}
+
+	async getTeams() {
+		return Team.find();
+	}
+
+	async getTeamById(teamId) {
+		return Team.findById(teamId).populate("players");
+	}
+
+	async getPlayers() {
+		return Player.find();
+	}
+
+	async getPlayerById(playerId) {
+		return Player.findById(playerId);
+	}
+
+	async getMatchScore(matchId) {
+		return Match.findById(matchId)
+			.select("status teamA teamB score currentInnings")
+			.populate("teamA")
+			.populate("teamB");
+	}
+	async getMatchScorecard(matchId) {
+		return Match.findById(matchId)
+			.populate("teamA")
+			.populate("teamB")
+			.populate("playing11.player");
+	}
+
+	async getMatchCommentary(matchId) {
+		return Commentary.find({
+			match: matchId,
+		})
+			.sort({ createdAt: -1 })
+			.populate("player");
+	}
+}
+
+module.exports = PublicRepository;
